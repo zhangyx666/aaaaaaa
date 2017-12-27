b7f318bb6ca60ccd934021e52588fc8b863f6daa
zhangxp <zhangxp@9186.com>
2017-12-27 10:36:36 +0800

    111

--- main/src/Test.java
+++ main/src/Test.java
@@ -4,22 +4,5 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
-        System.out.println("这是测试");
     }
 }
