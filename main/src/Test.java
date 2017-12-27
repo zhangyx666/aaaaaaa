37c6d641d31de4e642cda9ab35faa6452903d819
zhangyx666 <1141132724@qq.com>
2017-12-27 10:27:38 +0800

    Update Test.java

--- main/src/Test.java
+++ main/src/Test.java
@@ -4,5 +4,22 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
     }
 }
