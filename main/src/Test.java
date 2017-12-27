296465ae408260fc4628bddd16373c8e4e588014
zhangxp <zhangxp@9186.com>
2017-12-27 10:14:20 +0800

    111

--- main/src/Test.java
+++ main/src/Test.java
@@ -4,5 +4,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
+        System.out.println("这是测试");
     }
 }
